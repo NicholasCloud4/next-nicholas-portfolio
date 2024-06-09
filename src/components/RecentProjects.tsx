8d09ec2457dfcd5dc4a9bc6652510b077098003c
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-06-09 17:18:55 -0400

    added a tag

--- src/components/RecentProjects.tsx
+++ src/components/RecentProjects.tsx
@@ -16,64 +16,66 @@ const RecentProjects = () => {
                         key={id}
                         className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
                     >
-                        <PinContainer
-                            title={link}
-                            href={link}
-                            className="text-indigo-50"
-                        >
-                            <div
-                                className="relative flex items-center justify-center sm:w-[570px] w-[80vw]
-                             overflow-hidden sm:h-[40vh] h-[30vh] mb-10"
+                        <a href={link}>
+                            <PinContainer
+                                title={link}
+                                href={link}
+                                className="text-indigo-50"
                             >
-                                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#ffffff]">
-                                    <img src="/bg.png" alt="bg-img" />
-                                </div>
-                                <img
-                                    src={img}
-                                    alt={title}
-                                    className="z-10 absolute bottom-0"
-                                />
-                            </div>
-                            <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                                {title}
-                            </h1>
-
-                            <p className="lg:text-xl lg:font-normal font-light text-sm text-black-100">
-                                {des}
-                            </p>
-
-                            <div className="flex items-center justify-between mt-7 mb-3">
-                                <div className="flex items-center">
-                                    {iconLists.map((icon, index) => (
-                                        <div
-                                            key={icon}
-                                            className="bg-gray-200 border border-blue-500 rounded-full lg:w-10 lg:h-10 w-8 h-8
-                                            flex justify-center items-center"
-                                            style={{
-                                                transform: `translateX(-${
-                                                    5 * index + 2
-                                                }px)`,
-                                            }}
-                                        >
-                                            <img
-                                                src={icon}
-                                                alt={icon}
-                                                className="p-2"
-                                            />
-                                        </div>
-                                    ))}
-                                </div>
-                                <div className="flex justify-center items-center">
-                                    <p className="text-blue-500 flex lg:text-xl md:text-xs text-sm">
-                                        Visit Git Hub
-                                    </p>
-                                    <FaLocationArrow
-                                        className="ms-3"
-                                        color="#3b82f6"
+                                <div
+                                    className="relative flex items-center justify-center sm:w-[570px] w-[80vw]
+                             overflow-hidden sm:h-[40vh] h-[30vh] mb-10"
+                                >
+                                    <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#ffffff]">
+                                        <img src="/bg.png" alt="bg-img" />
+                                    </div>
+                                    <img
+                                        src={img}
+                                        alt={title}
+                                        className="z-10 absolute bottom-0"
                                     />
                                 </div>
-                            </div>
-                        </PinContainer>
+                                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                                    {title}
+                                </h1>
+
+                                <p className="lg:text-xl lg:font-normal font-light text-sm text-black-100">
+                                    {des}
+                                </p>
+
+                                <div className="flex items-center justify-between mt-7 mb-3">
+                                    <div className="flex items-center">
+                                        {iconLists.map((icon, index) => (
+                                            <div
+                                                key={icon}
+                                                className="bg-gray-200 border border-blue-500 rounded-full lg:w-10 lg:h-10 w-8 h-8
+                                            flex justify-center items-center"
+                                                style={{
+                                                    transform: `translateX(-${
+                                                        5 * index + 2
+                                                    }px)`,
+                                                }}
+                                            >
+                                                <img
+                                                    src={icon}
+                                                    alt={icon}
+                                                    className="p-2"
+                                                />
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className="flex justify-center items-center">
+                                        <p className="text-blue-500 flex lg:text-xl md:text-xs text-sm">
+                                            Visit Git Hub
+                                        </p>
+                                        <FaLocationArrow
+                                            className="ms-3"
+                                            color="#3b82f6"
+                                        />
+                                    </div>
+                                </div>
+                            </PinContainer>
+                        </a>
                     </div>
                 ))}
             </div>
