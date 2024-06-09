492190d3f1a7055eed7a758b688e99b571d83afc
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-06-09 17:07:30 -0400

    Projects

---
+++ src/components/RecentProjects.tsx
@@ -0,0 +1,84 @@
+import { projects } from "@/data";
+import React from "react";
+import { PinContainer } from "./ui/3d-pin";
+import { FaLocationArrow } from "react-icons/fa";
+
+const RecentProjects = () => {
+    return (
+        <div className="py-20" id="projects">
+            <h1 className="heading">
+                Some of my{" "}
+                <span className="text-blue-500">recent projects</span>
+            </h1>
+            <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+                {projects.map(({ id, title, des, img, iconLists, link }) => (
+                    <div
+                        key={id}
+                        className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
+                    >
+                        <PinContainer
+                            title={link}
+                            href={link}
+                            className="text-indigo-50"
+                        >
+                            <div
+                                className="relative flex items-center justify-center sm:w-[570px] w-[80vw]
+                             overflow-hidden sm:h-[40vh] h-[30vh] mb-10"
+                            >
+                                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#ffffff]">
+                                    <img src="/bg.png" alt="bg-img" />
+                                </div>
+                                <img
+                                    src={img}
+                                    alt={title}
+                                    className="z-10 absolute bottom-0"
+                                />
+                            </div>
+                            <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                                {title}
+                            </h1>
+
+                            <p className="lg:text-xl lg:font-normal font-light text-sm text-black-100">
+                                {des}
+                            </p>
+
+                            <div className="flex items-center justify-between mt-7 mb-3">
+                                <div className="flex items-center">
+                                    {iconLists.map((icon, index) => (
+                                        <div
+                                            key={icon}
+                                            className="bg-gray-200 border border-blue-500 rounded-full lg:w-10 lg:h-10 w-8 h-8
+                                            flex justify-center items-center"
+                                            style={{
+                                                transform: `translateX(-${
+                                                    5 * index + 2
+                                                }px)`,
+                                            }}
+                                        >
+                                            <img
+                                                src={icon}
+                                                alt={icon}
+                                                className="p-2"
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="flex justify-center items-center">
+                                    <p className="text-blue-500 flex lg:text-xl md:text-xs text-sm">
+                                        Visit Git Hub
+                                    </p>
+                                    <FaLocationArrow
+                                        className="ms-3"
+                                        color="#3b82f6"
+                                    />
+                                </div>
+                            </div>
+                        </PinContainer>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default RecentProjects;
--- src/data/index.ts
+++ src/data/index.ts
@@ -72,15 +72,15 @@ export const gridItems = [
 export const projects = [
     {
         id: 1,
-        title: "3D Solar System Planets to Explore",
+        title: "Threads Clone",
         des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
         img: "/p1.svg",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        link: "https://github.com/adrianhajdin?tab=repositories",
+        link: "https://github.com/NicholasCloud4/Threads-clone",
     },
     {
         id: 2,
-        title: "Yoom - Video Conferencing App",
+        title: "Netflix Clone",
         des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
         img: "/p2.svg",
         iconLists: [
@@ -90,29 +90,7 @@ export const projects = [
             "/stream.svg",
             "/c.svg",
         ],
-        link: "https://github.com/adrianhajdin/zoom-clone",
-    },
-    {
-        id: 3,
-        title: "AI Image SaaS - Canva Application",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/p3.svg",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "https://github.com/adrianhajdin/ai_saas_app",
-    },
-    {
-        id: 4,
-        title: "Animated Apple Iphone 3D Website",
-        des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-        img: "/p4.svg",
-        iconLists: [
-            "/next.svg",
-            "/tail.svg",
-            "/ts.svg",
-            "/three.svg",
-            "/gsap.svg",
-        ],
-        link: "https://github.com/adrianhajdin/iphone",
+        link: "https://github.com/NicholasCloud4/netflix",
     },
 ];
 
